c6cd52aaa35653e507c255e3351424de407e1b2b
skline <spencer.kline@gmail.com>
2011-03-06 13:20:47 -0500

    Handling invalid sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,17 @@ class SessionsController < ApplicationController
   def new
     @title= "Sign in"
   end
+  
   def create
+    user = user = User.authenticate(params[:session][:email],
+                                    params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination"
+      @title = "Sign in"
+    render 'new'
+    else
+    # handle success
+    end
   end
   
   def destroy
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,1 @@
-require 'spec_helper'
 
-describe "Users" do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-    end
-  end
-end
